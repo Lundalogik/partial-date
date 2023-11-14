ae5745f9d7a84df6d83a5882f8682e5691a35c32
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-11-14 11:28:24 +0100

    chore: set up mocha, chai, and typescript

---
+++ src/index.spec.ts
@@ -0,0 +1,8 @@
+import { expect }  from 'chai';
+import { PartialDate } from './index';
+
+describe('PartialDate', () => {
+    it('is a function', () => {
+        expect(PartialDate).to.be.a('function');
+    });
+});
