ba34efea8b3bf16bf75cafd0d956bf5a364d2d4f
Adrian Schmidt <adrian.schmidt@lime.tech>
2023-11-15 16:54:11 +0100

    chore: make it possible to run the linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,12 @@
 /* eslint-env node */
 module.exports = {
     extends: ['@limetech/eslint-config'],
+    overrides: [
+        {
+            files: ['src/**/*.ts'],
+            rules: {
+                'no-undef': 'off', // Typescript takes care of this for us.
+            },
+        },
+    ],
 };
